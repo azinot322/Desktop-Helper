5e3b5fb60b22f019eef87c555e76adb4569a7ebd
Artem Makarov <artmakarov2002@mail.ru>
2021-03-15 17:04:01 +0700

    К кнопкам были привязаны картинки

--- dia.py
+++ dia.py
@@ -1,6 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setWindowFlags(QtCore.Qt.FramelessWindowHint)
@@ -9,14 +8,26 @@ class Ui_Form(object):
         self.dialog = QtWidgets.QTextBrowser(Form)
         self.dialog.setGeometry(QtCore.QRect(70, 20, 381, 311))
         self.dialog.setObjectName("dialog")
+        pixmicro = QtGui.QPixmap(r'micro.jpg')
+        pixmicro = pixmicro.scaled(41, 41)
+        iconmicro = QtGui.QIcon(pixmicro)
         self.micro = QtWidgets.QPushButton(Form)
         self.micro.setGeometry(QtCore.QRect(10, 340, 41, 41))
         self.micro.setText("")
         self.micro.setObjectName("micro")
+        self.micro.setIcon(iconmicro)
+        self.micro.setIconSize(pixmicro.rect().size())
+        self.micro.setFixedSize(pixmicro.rect().size())
+        pixset = QtGui.QPixmap(r'settings.png')
+        pixset = pixset.scaled(41, 31)
+        iconset = QtGui.QIcon(pixset)
         self.settings = QtWidgets.QPushButton(Form)
         self.settings.setGeometry(QtCore.QRect(10, 10, 41, 31))
         self.settings.setText("")
         self.settings.setObjectName("settings")
+        self.settings.setIcon(iconset)
+        self.settings.setIconSize(pixset.rect().size())
+        self.settings.setFixedSize(pixset.rect().size())
         self.lineEdit = QtWidgets.QLineEdit(Form)
         self.lineEdit.setGeometry(QtCore.QRect(100, 350, 301, 31))
         self.lineEdit.setStyleSheet("background-color:rgb(255, 255, 255);\n"
@@ -25,13 +36,17 @@ class Ui_Form(object):
                                     "\n""")
         self.lineEdit.setObjectName("lineEdit")
         self.lineEdit.setPlaceholderText("Введите текст")
+        pixsend = QtGui.QPixmap(r'send.jpg')
+        pixsend = pixsend.scaled(51, 41)
+        iconsend = QtGui.QIcon(pixsend)
         self.send = QtWidgets.QPushButton(Form)
         self.send.setGeometry(QtCore.QRect(430, 340, 51, 41))
         self.send.setText("")
         self.send.setObjectName("send")
+        self.send.setIcon(iconsend)
+        self.send.setIconSize(pixsend.rect().size())
+        self.send.setFixedSize(pixsend.rect().size())
         self.send.clicked.connect(self.output)
-
-
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
